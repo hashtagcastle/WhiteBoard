fa5e59c6919602d0d1d4cb45c1456b53ae8fff71
AmmoBear <awest@outlook.com>
2015-12-26 15:14:42 -0600

    Refactor code to create server module

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to White Board.\n');
-}).listen(port);
+var server = require("./server");
+
+server.start();
