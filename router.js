d83d5f67edef74824c5f8d2e7fca1c43e8494f00
AmmoBear <awest@outlook.com>
2015-12-30 12:03:22 -0800

    Lower case request path routing

--- router.js
+++ router.js
@@ -1,15 +1,17 @@
 function route(handle, pathname, request, response) {
     console.log("About to route a request for " + pathname + ".");
     
-    if( handle[request.method] && 
-        handle[request.method][pathname] &&
-        typeof handle[request.method][pathname] === 'function'
+    var requestMethod = request.method;
+    var sanitizedPathName = pathname.toLowerCase();
+    if( handle[requestMethod] && 
+        handle[requestMethod][sanitizedPathName] &&
+        typeof handle[requestMethod][sanitizedPathName] === 'function'
     )
     {
-        handle[request.method][pathname](request, response);
+        handle[requestMethod][sanitizedPathName](request, response);
     }
     else{
-        console.log("No request handler found for " + request.Method + " and request path " + pathname);
+        console.log("No request handler found for " + requestMethod + " and request path " + pathname);
         response.writeHead(404, {"Content-Type": "text/plain"});
         response.write("404 Not Found.");
         response.end();
